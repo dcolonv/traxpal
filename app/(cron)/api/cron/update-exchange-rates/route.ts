d1345366836053d4d2f2e11cbda539326ce16ef4
David Colon <dcolonv@gmail.com>
2024-08-27 12:00:10 +0200

    Remove security of cron api

--- app/(cron)/api/cron/update-exchange-rates/route.ts
+++ app/(cron)/api/cron/update-exchange-rates/route.ts
@@ -35,10 +35,10 @@ const getExchangeRates = async (
 };
 
 export async function GET(request: NextRequest) {
-  const authHeader = request.headers.get('authorization');
-  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
-    return new Response('Unauthorized', { status: 401 });
-  }
+  // const authHeader = request.headers.get('authorization');
+  // if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+  //   return new Response('Unauthorized', { status: 401 });
+  // }
 
   const yesterday = new Date();
   yesterday.setDate(yesterday.getDate() - 5);
